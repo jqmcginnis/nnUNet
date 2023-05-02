b09c62a294a30cced6e382765964a1840294c218
Julian McGinnis <julian.mcginnis@tum.de>
2023-05-02 14:34:22 +0200

    add FocalLoss based on JunMa11's SegLoss

---
+++ nnunetv2/training/nnUNetTrainer/variants/loss/nnUNetTrainerFocalLoss.py
@@ -0,0 +1,22 @@
+from nnunetv2.training.loss.deep_supervision import DeepSupervisionWrapper
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.training.loss.robust_ce_loss import RobustCrossEntropyLoss
+from nnunetv2.training.loss.focal_loss import FocalLoss
+
+import numpy as np
+
+class nnUNetTrainerFocalLoss(nnUNetTrainer):
+    def _build_loss(self):
+        assert not self.label_manager.has_regions, 'regions not supported by this trainer'
+        loss = FocalLoss()
+        deep_supervision_scales = self._get_deep_supervision_scales()
+
+        # we give each output a weight which decreases exponentially (division by 2) as the resolution decreases
+        # this gives higher resolution outputs more weight in the loss
+        weights = np.array([1 / (2 ** i) for i in range(len(deep_supervision_scales))])
+
+        # we don't use the lowest 2 outputs. Normalize weights so that they sum to 1
+        weights = weights / weights.sum()
+        # now wrap the loss
+        loss = DeepSupervisionWrapper(loss, weights)
+        return loss
